4c702d205847713f737091b97697dfc423ff5a8d
Saad Makrod <saad.makrod@gmail.com>
2024-10-06 21:16:14 -0400

    update partition labels

--- partition-labels/Solution.py
+++ partition-labels/Solution.py
@@ -2,28 +2,47 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        # last_seen = {}
+
+        # for i in range(len(s)):
+        #     last_seen[s[i]] = i
+
+        # curr = set(s[0])
+        # sol = []
+        # start = 0
+
+        # for i in range(1, len(s)):
+        #     clear = True
+
+        #     for char in curr:
+        #         if last_seen[char] >= i:
+        #             clear = False
+
+        #     if clear:
+        #         sol.append(i - start)
+        #         start = i
+        #         curr = set()
+
+        #     curr.add(s[i])
+
+        # sol.append(i - start + 1)
+        # return sol
+
         last_seen = {}
 
         for i in range(len(s)):
             last_seen[s[i]] = i
 
-        curr = set(s[0])
+        curr = 0
+        target = 0
         sol = []
-        start = 0
 
-        for i in range(1, len(s)):
-            clear = True
+        for i in range(len(s)):
+            target = max(last_seen[s[i]], target)
+            curr += 1
 
-            for char in curr:
-                if last_seen[char] >= i:
-                    clear = False
+            if i == target:
+                sol.append(curr)
+                curr = 0
 
-            if clear:
-                sol.append(i - start)
-                start = i
-                curr = set()
-
-            curr.add(s[i])
-
-        sol.append(i - start + 1)
         return sol
