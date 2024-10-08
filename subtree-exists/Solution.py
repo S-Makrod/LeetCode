a6aa28d75be2505616888407e13374b1c35efe10
Saad Makrod <saad.makrod@gmail.com>
2024-10-08 09:02:35 -0400

    adding subtree exists

---
+++ subtree-exists/Solution.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        if self.isSameTree(root, subRoot):
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if q is None and p is None:
+            return True
+        if q is None or p is None:
+            return False
+
+        return p.val == q.val and self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
