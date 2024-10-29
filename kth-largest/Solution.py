a61b6d6bae63fc852eb65aaa578cc2ee29f21fd7
Saad Makrod <saad.makrod@gmail.com>
2024-10-29 10:23:55 -0400

    update kth largest

--- kth-largest/Solution.py
+++ kth-largest/Solution.py
@@ -18,3 +18,15 @@ class KthLargest:
 
         return self.heap[0]
 
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+
+        for num in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
+            elif heap[0] < num:
+                heapq.heappushpop(heap, num)
+
+        return heap[0]
+
